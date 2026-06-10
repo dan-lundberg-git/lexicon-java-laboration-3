b5df0ed56e12057e318cdd5d1712ef98cc018b38
Dan Lundberg <dan-lundberg@outlook.com>
2026-06-10 10:14:39 +0200

    Add class Order and a method for displaying a menu.

--- src/main/java/CafeApp.java
+++ src/main/java/CafeApp.java
@@ -1,30 +1,64 @@
+import java.util.Scanner;
+
+/*
+ * 1. What are the different responsibilities in this application?
+ *
+ *       - Get and validate user input.
+ *       - Render the menu.
+ *       - Calculate a subtotal including possible discounts and VAT.
+ *       - Render the receipt.
+ *
+ * 2. Which responsibility is the easiest to implement first?
+ *
+ *       - Anything that has to do with pure input and output, i.e. logic void of calculations.
+ *
+ * 3. What information needs to flow from one part of the program to another?
+ *
+ *       - The order information, like item, price, amount and so on.
+ *
+ * 4. When a method needs information, should it calculate it itself or receive it as a parameter?
+ *
+ *       - I would suggest all information needed for the method's internal logic should
+ *         be supplied as parameters.
+ *
+ * 5. If a piece of logic feels difficult to explain, can it be broken into smaller steps?
+ *
+ *       - Probably yes. We'll see if we come to that scenario.
+ *
+ * */
+
 public class CafeApp {
+
+    static void getMenu(Order order) {
+        System.out.println("\nHi " + order.getCustomerName() + "! This is our menu:\n");
+        System.out.print("""
+                ==============================
+                        Lexicon Café
+                ==============================
+                1. Espresso          25.00 SEK
+                2. Cappuccino        35.00 SEK
+                3. Latte             40.00 SEK
+                4. Croissant         30.00 SEK
+                5. Sandwitch         50.00 SEK
+                ==============================
+                """);
+    }
+
     static void main() {
-        /*
-        * 1. What are the different responsibilities in this application?
-        *
-        *       - Get and validate user input.
-        *       - Render the menu.
-        *       - Calculate a subtotal including possible discounts and VAT.
-        *       - Render the receipt.
-        *
-        * 2. Which responsibility is the easiest to implement first?
-        *
-        *       - Anything that has to do with pure input and output, i.e. logic void of calculations.
-        *
-        * 3. What information needs to flow from one part of the program to another?
-        *
-        *       - The order information, like item, price, amount and so on.
-        *
-        * 4. When a method needs information, should it calculate it itself or receive it as a parameter?
-        *
-        *       - I would suggest all information needed for the method's internal logic should
-        *         be supplied as parameters.
-        *
-        * 5. If a piece of logic feels difficult to explain, can it be broken into smaller steps?
-        *
-        *       - Probably yes. We'll see if we come to that scenario.
-        *
-        * */
+
+        // Prepare the order with an empty order object.
+        Order order = new Order();
+
+        // Prepare for user input with a Scanner object.
+        Scanner scanner = new Scanner(System.in);
+
+        // Get user input.
+        System.out.print("Welcome! What is your name? ");
+        order.setCustomerName(scanner.nextLine());
+
+        // Render the menu.
+        getMenu(order);
+
+
     }
 }
---
+++ src/main/java/Order.java
@@ -0,0 +1,59 @@
+public class Order {
+    private String customerName;
+    private String itemName;
+    private int itemQuantity;
+    private boolean isMember;
+    private double itemPrice;
+    private double subTotal;
+
+    public Order() {
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+
+    public void setMember(boolean member) {
+        isMember = member;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(double subTotal) {
+        this.subTotal = subTotal;
+    }
+}
