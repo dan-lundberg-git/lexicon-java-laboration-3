6f33ba7e9bc4ff6fc31f3f5a04b23850d39de00c
Dan Lundberg <dan-lundberg@outlook.com>
2026-06-10 09:37:56 +0200

    Add initial thoughts on the project. Responsibilites, logical flow etcetera.

--- src/main/java/CafeApp.java
+++ src/main/java/CafeApp.java
@@ -1,5 +1,30 @@
 public class CafeApp {
     static void main() {
-
+        /*
+        * 1. What are the different responsibilities in this application?
+        *
+        *       - Get and validate user input.
+        *       - Render the menu.
+        *       - Calculate a subtotal including possible discounts and VAT.
+        *       - Render the receipt.
+        *
+        * 2. Which responsibility is the easiest to implement first?
+        *
+        *       - Anything that has to do with pure input and output, i.e. logic void of calculations.
+        *
+        * 3. What information needs to flow from one part of the program to another?
+        *
+        *       - The order information, like item, price, amount and so on.
+        *
+        * 4. When a method needs information, should it calculate it itself or receive it as a parameter?
+        *
+        *       - I would suggest all information needed for the method's internal logic should
+        *         be supplied as parameters.
+        *
+        * 5. If a piece of logic feels difficult to explain, can it be broken into smaller steps?
+        *
+        *       - Probably yes. We'll see if we come to that scenario.
+        *
+        * */
     }
 }
